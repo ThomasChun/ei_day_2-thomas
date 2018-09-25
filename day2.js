90f6c13a48b1d2c729718887d59c5a78bccd78fa
Thomas Chun <tchun2055@gmail.com>
2018-09-25 11:14:14 -0700

    added functions as arguments (2)

--- day2.js
+++ day2.js
@@ -2,7 +2,6 @@
 
 // Redo the "max and min (without sort or Math.max/min)" drill but this time use 
 // a while loop instead of a for loop
-
 function max(numbers) {
   let maxNum = numbers[0];
   let i = 0;
@@ -28,20 +27,21 @@ function min(numbers) {
 }
 
 
+
 // Redo "Compute the average" exercise but use the Array.forEach() method instead 
 // of a for loop
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
-
 function average(numbers) {
   let arraySum = 0;
   numbers.forEach(element => arraySum += element);
   return arraySum / numbers.length;
 }
 
+
+
 // Higher Order Function Drills
 
 // Functions as arguments (1)
-
 function repeat(fn, n) {
   for (let i = 0; i < n; i++) {
     fn();
@@ -55,5 +55,30 @@ let goodbye = () => console.log('Goodbye world');
 repeat(hello, 3);
 repeat(goodbye, 5);
 
-// Functions as arguments (2)
 
+
+// Functions as arguments (2) 
+function filter(arr, fn) {
+  const newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (fn(arr[i]) === true) {
+      newArray.push(arr[i]);
+    }
+  }
+  return newArray;
+}
+
+// DO NOT EDIT BETWEEN THESE LINES, BUT DO READ THE CODE ----->
+// First we setup an array of strings we plan to filter:
+const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+
+// We use your `filter` function here, capturing a new array into `filteredNames`
+// comprised of names that only begin with 'R'
+const filteredNames = filter(myNames, function(name) {
+  // This is known as a "predicate function" - it's a function that 
+  // only returns a boolean
+  return name[0] === 'R';
+});
+
+console.log(filteredNames) // => ['Rich', 'Ray']
+// <---- DO NOT EDIT BETWEEN THESE LINES
